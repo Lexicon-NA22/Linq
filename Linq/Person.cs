e8987b70048d42a7acf07c8227dd603585501f22
Dimitris Björlingh <dimitris.bjorlingh@outlook.com>
2021-12-16 15:22:10 +0100

    GetPersons, Person

---
+++ Linq/Person.cs
@@ -0,0 +1,17 @@
+﻿namespace Linq
+{
+    internal class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public Person(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+
+        public override string ToString() => $"Name: {Name} Age: {Age}";
+      
+    }
+}
